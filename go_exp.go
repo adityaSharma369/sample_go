9124701c24492b0200127da0ffd12062145cbaa3
tericnexus <adityadamera369@gmail.com>
2019-11-28 19:01:25 +0530

    2 commit

--- go_exp.go
+++ go_exp.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	//declaring a integer variable x
 	var x int
-	x = 2                //assigning x the value 3
+	x = 3                //assigning x the value 3
 	fmt.Println("x:", x) //prints 3
 
 	//declaring a integer variable y with value 20 in a single statement and prints it
-	var y int = 10
+	var y int = 25
 	fmt.Println("y:", y)
 
 	//declaring a variable z with value 50 and prints it
