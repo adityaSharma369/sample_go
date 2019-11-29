e5a1ff6b99224b93e46bdb66dc0a03fd0b5738cd
tericnexus <adityadamera369@gmail.com>
2019-11-29 11:21:09 +0530

    4 commit

--- go_exp.go
+++ go_exp.go
@@ -5,19 +5,19 @@ import "fmt"
 func main() {
 	//declaring a integer variable x
 	var x int
-	x = 3                //assigning x the value 3
+	x = 4                //assigning x the value 3
 	fmt.Println("x:", x) //prints 3
 
 	//declaring a integer variable y with value 20 in a single statement and prints it
-	var y int = 20
+	var y int = 23
 	fmt.Println("y:", y)
 
 	//declaring a variable z with value 50 and prints it
 	//Here type int is not explicitly mentioned
-	var z = 15
+	var z = 29
 	fmt.Println("z:", z)
 
 	//Multiple variables are assigned in single line- i with an integer and j with a string
-	var i, j = 49, "hello"
+	var i, j = 52, "hello"
 	fmt.Println("i and j:", i, j)
 }
