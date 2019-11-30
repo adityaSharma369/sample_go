44a32739368452ce1e2dce8c5a9b53fd243200ee
tericnexus <adityadamera369@gmail.com>
2019-11-30 18:31:22 +0530

    modified exp

--- go_exp.go
+++ go_exp.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("test jenkins")
+	fmt.Println("its jenkins world")
 	//declaring a integer variable x
 	var x int
-	x = 4                //assigning x the value 3
+	x = 3                //assigning x the value 3
 	fmt.Println("x:", x) //prints 3
 
 	//declaring a integer variable y with value 20 in a single statement and prints it
-	var y int = 51
+	var y int = 82
 	fmt.Println("y:", y)
 
 	//declaring a variable z with value 50 and prints it
