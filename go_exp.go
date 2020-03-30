0b4b7f23d9a1bcd8df7b31e33a117b21e6ee1181
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-03-30 15:18:23 +0530

    feat:lets check it

--- go_exp.go
+++ go_exp.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println("x:", x) //prints 3
 
 	//declaring a integer variable y with value 20 in a single statement and prints it
-	var y int = 82
+	var y int = 80
 	fmt.Println("y:", y)
 
 	//declaring a variable z with value 50 and prints it
