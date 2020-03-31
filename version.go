386e42fe8eeaba6918a6ba984e9ff319b12dd256
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-03-31 14:25:28 +0530

    feat:added some changes

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ func main() {
     version = os.Getenv("Version")
     app := "go"
     arg0 := "run"
-    arg1 := -ldflags
-    arg2 := "-X main.Version="+verion
+    arg1 := "-ldflags"
+    arg2 := "-X main.Version="+version
     cmd := exec.Command(app, arg0, arg1, arg2)
 }
