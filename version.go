24fc7cd2734e769438c7c137f1e5e20b8ec3aa82
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-03-31 18:11:19 +0530

    lets try

--- version.go
+++ version.go
@@ -1,18 +1,25 @@
 package main
 import (
-    "github.com/joho/godotenv"
+//     "github.com/joho/godotenv"
     "fmt"
     "os"
-"os/exec"
+// "os/exec"
 )
 
 func main() {
 	
-  err := godotenv.Load()
-  if err != nil {
-    fmt.Println("Error loading .env file")
-  }
-    version := os.Getenv("INPUT_VERSION")
+//   err := godotenv.Load()
+//   if err != nil {
+//     fmt.Println("Error loading .env file")
+//   }
+// Open our jsonFile
+jsonFile, err := os.Open("package.json")
+// if we os.Open returns an error then handle it
+if err != nil {
+    fmt.Println(err)
+}
+	 version := jsonFile.version
+//     version := os.Getenv("INPUT_VERSION")
     fmt.Println("version----",version)
 	cmd := exec.Command("go build -ldflags=\"-X 'main.Version="+version+"'\" go_exp.go")
 	cmd.Start()
