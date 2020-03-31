a153abffcf7521e445c7c835184d92ee2b909a65
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-03-31 16:33:55 +0530

    change

--- go_exp.go
+++ go_exp.go
@@ -1,12 +1,27 @@
 package main
+import (
+    "github.com/joho/godotenv"
+    "fmt"
+    "os"
+"os/exec"
+)
 
-import "fmt"
 
 func main() {
 	var (
     		version string
    	        
 	    )
+	  err := godotenv.Load()
+  if err != nil {
+    fmt.Println("Error loading .env file")
+  }
+    version := os.Getenv("INPUT_VERSION")
+    fmt.Println("version----",version)
+// 	cmd := exec.Command("go build -ldflags=\"-X 'main.Version="+version+"'\" go_exp.go")
+// 	cmd.Start()
+// 	fmt.Println(cmd)
+}
 
 	fmt.Println("version=", version)
 	fmt.Println("its jenkins world")
