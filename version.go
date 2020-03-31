99f308699ba57f27ae44f3e44ff75ab464a5fee9
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-03-31 14:39:02 +0530

    feat:added somefeat

--- version.go
+++ version.go
@@ -2,12 +2,17 @@ package main
 
 import "os/exec"
 import "os"
+import "fmt"
 
 func main() {
     version := os.Getenv("Version")
+    fmt.Println("version----",version)
     app := "go"
     arg0 := "run"
     arg1 := "-ldflags"
     arg2 := "-X main.Version="+version
+    fmt.Println("arg2=",arg2)
     exec.Command(app, arg0, arg1, arg2)
 }
+
+
