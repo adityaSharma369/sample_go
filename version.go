a4cfbd89b32a83f41da38ef1ac6d43184e3b4ea6
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-03-31 14:20:56 +0530

    feat:new version.go

---
+++ version.go
@@ -0,0 +1,13 @@
+package main
+
+import "os/exec"
+import "os"
+
+func main() {
+    version = os.Getenv("Version")
+    app := "go"
+    arg0 := "run"
+    arg1 := -ldflags
+    arg2 := "-X main.Version="+verion
+    cmd := exec.Command(app, arg0, arg1, arg2)
+}
